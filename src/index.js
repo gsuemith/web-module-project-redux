93522367ee16c0edb65f923f03e32a07db0ab145
Garrick Suemith <garrick.suemith@gmail.com>
2021-03-09 12:12:09 -0500

    created reducer

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'react-redux'
 import App from './App';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+import rootReducer from './reducers'
+
+const store = createStore(rootReducer);
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <App />
+  , 
+  rootElement
+  );
